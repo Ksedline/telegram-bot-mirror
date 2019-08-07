ffae44af0a7be39610fb990bbface6342ba4926e
vladcyb <vladtsibizov@gmail.com>
2019-08-07 21:56:51 +0300

    res.body

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const sendRequest = function(token, action, form, callback){
     if(err){
       return callback({ ok: false, err: err })
     }
-    callback({ ok: true, res: res })
+    callback({ ok: true, res: res.body })
   })
 }
 
